e3aa5ac5b27b436701a33e9c3167b663b933bff0
simituli <anupama.tuli@gmail.com>
2024-12-09 17:30:50 -0800

    Update sample.java - Solve https://leetcode.com/problems/evaluate-reverse-polish-notation/
    https://leetcode.com/problems/evaluate-reverse-polish-notation/

--- sample.java
+++ sample.java
@@ -41,3 +41,60 @@ class Solution {
 }
 
 // Your code here along with comments explaining your approach
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No, compilation errors and corner cases
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        Set<String> set = new HashSet<>();
+        int maxValue = Integer.MAX_VALUE;
+        int i = maxValue;
+        set.add("+");
+        set.add("-");
+        set.add("*");
+        set.add("/");
+        for(String token: tokens)
+        {
+            i = maxValue;
+            try
+            {
+                i = Integer.parseInt(token);
+                
+            }
+            catch(Exception ex)
+            {
+                
+            }
+            if(i!=maxValue)
+            {
+                st.push(i);
+            }
+            else if(!st.isEmpty() && st.size()>1 && set.contains(token))
+                {   
+                    int first = st.pop();
+                    int second = st.pop();
+                    if(token.equals("+"))
+                    {
+                        st.push(first+second);
+                    }
+                    if(token.equals("-"))
+                    {
+                        st.push(second-first);
+                    }
+                    if(token.equals("*"))
+                    {
+                        st.push(first*second);
+                    }
+                    if(token.equals("/") && first!=0)
+                    {
+                        st.push(second/first);
+                    }
+                }     
+            }
+            if(!st.isEmpty()) return st.pop();
+            else return -1;
+        }  
+    }
+
